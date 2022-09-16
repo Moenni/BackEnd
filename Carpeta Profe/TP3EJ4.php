b132a9ff08af8e0ae2d9eb9c6b63bc6576ca4dbd
Moenni <nicolasmoen@live.com>
2022-09-16 00:40:20 -0300

    trabajando en tp3ej4

---
+++ Carpeta Profe/TP3EJ4.php
@@ -0,0 +1,7 @@
+<?php
+
+/*4. Imprimir un listado con los números impares desde 1 hasta 999 y seguidamente otro listado con los
+números pares desde 2 hasta 1000*/
+for ($i=1;$i<=999; $i+=2) {
+    echo "$i". PHP_EOL;
+}
