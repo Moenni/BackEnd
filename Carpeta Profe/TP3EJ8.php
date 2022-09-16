9c7f4bfa1faa3d770dece8f1cc70f1eaa4f36fdd
Moenni <nicolasmoen@live.com>
2022-09-16 01:26:06 -0300

    agrego contenido a TP3ej8

--- Carpeta Profe/TP3EJ8.php
+++ Carpeta Profe/TP3EJ8.php
@@ -2,3 +2,15 @@
 
 /*8. Obtener el resto de la división entera a%b de dos números enteros positivos mediante restas.*/
 
+
+$A=readline("Ingrese el valor de A: ");
+$B=readline("Ingrese el valor de B: ");
+
+$acumuladora=$A;
+$contadora=0;
+
+while($acumuladora>=$B){
+    $acumuladora-=$B;//$acumuladora= $acumuladora -$B;
+    $contadora++;
+}
+echo "$A / $B = $acumuladora";
