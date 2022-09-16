0ddd73ece77982bca52b90b227a602c123289b2b
Moenni <nicolasmoen@live.com>
2022-09-16 01:53:33 -0300

    agrego TP3EJ9

---
+++ Carpeta Profe/TP3/TP3EJ9.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Imprimir los números de 1 a N (siendo N un número que se lee) cada uno con su respectivo factorial.*/
+/*
+9!=1x2x3x4x5x6x7x8x9
+N!=Nx(N-1)X(N-2)....
+N!=NX(N-1)!
+*/
+
+$N = readline("ingrese el valor de N: ");
+$acumulador=1;
+ for($i=1;$i<=$N;$i++){
+    $acumulador*=$i;
+ }
+echo "N!= $acumulador"; //1*2*3*4*5
