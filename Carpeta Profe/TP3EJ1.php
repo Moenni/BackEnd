fcd14c343e0586bc61b5ba7d7008b71e9807627d
Moenni <nicolasmoen@live.com>
2022-09-16 00:13:03 -0300

    trabajando en el tp3

--- Carpeta Profe/TP3EJ1.php
+++ Carpeta Profe/TP3EJ1.php
@@ -8,7 +8,7 @@ promedio= suma/cantidad
 
 
 */
-
+/*
 $suma=0;
 $cantidad=0;
 $nota= readline("ingrese una nota o una nota negativa para salir: ");
@@ -19,3 +19,34 @@ while($nota>=0){
 }
 $promedio= $suma/$cantidad;
 echo "el promedio de los numeros ingresados es : $promedio";
+*/
+/*
+$suma=0;
+$cantidad=0;
+$nota= readline("ingrese una nota o una nota negativa para salir: ");
+if($nota>=0){
+do{
+
+    $suma+= $nota;
+    $cantidad++;
+    $nota= readline("ingrese una nota o una nota negativa para salir: ");
+}while($nota>=0){
+$promedio= $suma/$cantidad;
+echo "el promedio de los numeros ingresados es : $promedio";
+*/
+
+$suma=0;
+$cantidad=0;
+for ($nota=readline("ingrese una nota: ");$nota>=0;$nota=readline("ingrese una nota: ") ) {
+    $cantidad++; //$cantidad= $cantidad +1
+    
+    $suma+=$nota;
+}
+/*
+if($cantidad!=0){
+    $promedio=$suma/$cantidad; 
+}
+*/
+//usamos operador ternario
+$promedio= ($cantidad!=0) ? $suma/$cantidad: 0;
+echo "El promedio de los numeros ingresados es : $promedio";
---
+++ Carpeta Profe/TP3EJ2.php
@@ -0,0 +1,32 @@
+<?php
+
+/*Realizar la multiplicacion de dos numeros enteros A y B mediante sumas sucesivas , hacer tres algoritmos con cada estructura repetitiva*/
+
+// 6*4 = 6+6+6+6 =24
+//A*B=A+A+A+A = B+B+B+B
+
+$A=readline("Ingrese el valor de A: ");
+$B=readline("Ingrese el valor de B: ");
+
+
+if($B<0){
+    $B*=-1; //$B= $
+}
+/*
+echo "$A X $B = $acumuladora";$acumuladora=0;
+$contadora=0;
+
+while ($contadora<$B) {
+    $acumuladora+=$A;  //$acumuladora= $acumuladora+$A;
+    $contadora++; //$contadora=$contadora+1;
+
+}
+
+echo "$A X $B = $acumuladora";
+*/
+$acumuladora=0;
+for($contadora =0;$contadora<$B; $contadora++){
+    $acumuladora+=$A;
+}
+echo "$A X $B = $acumuladora";
+
