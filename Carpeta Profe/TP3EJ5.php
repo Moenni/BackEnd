6ed693420517fb3dc2b7f044096ee4e8decf4f2c
Moenni <nicolasmoen@live.com>
2022-09-16 00:58:11 -0300

    agrego tp3ej5

---
+++ Carpeta Profe/TP3EJ5.php
@@ -0,0 +1,13 @@
+<?php
+
+/*Imprimir los números pares desde N (siendo N un número par que se lee) en forma descendente
+hasta 2.*/
+
+$N=readline("Ingrese N: ");
+if ($N % 2 == 0) {
+    for (;$N>=2;$N-=2) {
+        echo "N = $N". PHP_EOL;
+    }
+}else{
+    echo "El valor ingresado de N no es par";
+}
