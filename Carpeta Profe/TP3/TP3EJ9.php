e9c2d7deeff970f9c77b737b6c5cc0de40e3e656
Moenni <nicolasmoen@live.com>
2022-09-16 02:11:01 -0300

    agrego tp3ej10

--- Carpeta Profe/TP3/TP3EJ9.php
+++ Carpeta Profe/TP3/TP3EJ9.php
@@ -11,5 +11,11 @@ $N = readline("ingrese el valor de N: ");
 $acumulador=1;
  for($i=1;$i<=$N;$i++){
     $acumulador*=$i;
+    //1*1
+    //1*2
+    //2*3
+    //6*4
+    //24*5
+    //120
+    echo "$i!= $acumulador". PHP_EOL; //1*2*3*4*5
  }
-echo "N!= $acumulador"; //1*2*3*4*5
